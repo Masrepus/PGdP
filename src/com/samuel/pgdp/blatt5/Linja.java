62b20174b1ee951fd9d835f0ce7f77bfa26350f5
Samuel <masrepus97@gmail.com>
2016-11-25 13:59:17 +0100

    implemented bonus rule for 5.4

--- src/com/samuel/pgdp/blatt5/Linja.java
+++ src/com/samuel/pgdp/blatt5/Linja.java
@@ -4,6 +4,10 @@ import com.samuel.pgdp.MiniJava;
 
 public class Linja extends MiniJava {
 
+    /**
+     * numberPieces contains the number of pieces still in the game, not Displayed the ones that are not there anymore.
+     * Those two arrays HAVE to be updated if a piece exits the visible game area!
+     */
     private static int[][] spielfeld = new int[8][6];
     private static int[] numberPieces = new int[]{12,12}; //0 is player -1 and 1 is player 1
     private static int[] notDisplayed = new int[2]; //0 is player -1 and 1 is player 1
@@ -101,18 +105,28 @@ public class Linja extends MiniJava {
      * wurde genau getroffen (potentieller Bonuszug)
      */
     private static int setzeZug(int stein, int weite, boolean vorwaerts) {
-        //check where stein is and what the destination would be
+        //check where the piece is right now
         int[] currPosition = findeStein(stein);
         if (currPosition[0] == -1 && currPosition[1] == -1) return -1;
 
+        //which row is the "finish line" and the "starting point" for this piece?
         int finishLine = (stein < 0) ? 0 : 7;
+        int start = (stein < 0) ? 7 : 0;
 
-        //you also can't move a piece one row forward if it has already crossed the finish line
-        if (vorwaerts && currPosition[0] == finishLine) return -1;
-
+        //calculate the destination row
         int destRow;
-        if (stein < 0) destRow = currPosition[0] - weite;
-        else destRow = currPosition[0] + weite;
+        if (vorwaerts) {
+            if (stein < 0) destRow = currPosition[0] - weite;
+            else destRow = currPosition[0] + weite;
+        } else {
+            if (stein < 0) destRow = currPosition[0] + weite;
+            else destRow = currPosition[0] - weite;
+        }
+
+        //you can't move a piece if it has already crossed the finish line
+        if (currPosition[0] == finishLine) return -1;
+        //you can't move a piece backward if it is still in the starting row
+        else if (!vorwaerts && currPosition[0] == start) return -1;
 
         //check if destRow is still between 0 and 7 or exactly 7 (potential bonus round)
         if (stein > 0 && destRow > 7 || stein < 0 && destRow < 0) {
@@ -123,6 +137,7 @@ public class Linja extends MiniJava {
             if (stein < 0) notDisplayed[0]++;
             else notDisplayed[1]++;
 
+            //decrease the number of pieces stil in the game
             if (stein < 0) numberPieces[0]--;
             else numberPieces[1]--;
             return 0;
@@ -172,7 +187,7 @@ public class Linja extends MiniJava {
 
         for (int i = 0; i < spielfeld.length; i++) {
             for (int j = 0; j < spielfeld[i].length; j++) {
-                if (number1 == numberPieces[1] || numberMinus1 == numberPieces[0]) return true; //we found all pieces of one color without finding one of the other color
+                if (number1 == numberPieces[1] || numberMinus1 == numberPieces[0]) return true; //we found all pieces of one color without finding one of the other color ==> end of game
                 if (spielfeld[i][j] > 0) {
                     if (onlyPlayer == -1) return false;
                     else onlyPlayer = 1;
@@ -235,8 +250,9 @@ public class Linja extends MiniJava {
      *
      * @param spieler ist 1 (Spielsteine 1 bis 12) oder -1 (Spielsteine -1 bis
      *                -12)
+     * @param bonus whether we use the bonus rule
      */
-    private static void spielerZieht(int spieler) {
+    private static void spielerZieht(int spieler, boolean bonus) {
         //prompt the player to choose a piece
         String msg = "Please choose which piece you want to move. You are player " + spieler + ", so your pieces are numbered from ";
         if (spieler == 1) msg += "1 to 12";
@@ -258,7 +274,10 @@ public class Linja extends MiniJava {
         System.out.println(output());
 
         //finally the player chose a move that could be executed. Now he possibly gets another one
-        if (nextTurn > 0) {
+
+        //check if this is a bonus round
+        if (bonus && nextTurn == 6) handleBonus(spieler);
+        else if (nextTurn > 0) {
             //prompt the player to choose a piece
             msg += "\n You have another move. Distance: " + nextTurn;
             currPiece = readInt(msg);
@@ -270,12 +289,52 @@ public class Linja extends MiniJava {
                 legal = gueltigeEingabe(currPiece, spieler);
             }
 
-            //this time a possible next move is dismissed
-            performMove(currPiece, nextTurn, spieler, msg);
+            //this time a possible next move is dismissed, unless we get a bonus move (if we use the bonus rule)
+            nextTurn = performMove(currPiece, nextTurn, spieler, msg);
 
             //print the current game status
             System.out.println(output());
+
+            if (bonus && nextTurn == 6) handleBonus(spieler);
         }
+    }
+
+    private static void handleBonus(int player) {
+
+        int code;
+
+        do {
+            //prompt the player to choose a piece they would like to use for their bonus round
+            String msg = "You get a bonus round! You are now allowed to move one piece (one of own or one of your opponents) - either one row forward or one row backward. First choose which piece you would like to move\n" +
+                    "You are player " + player + ", so your pieces are numbered from " + ((player == -1) ? "-1 to -12." : "1 to 12.");
+            int currPiece = readInt(msg);
+
+            //check if the choice was legal, this time pieces from every player can be used
+            while (!(currPiece >= -12 && currPiece != 0 && currPiece <= 12)) {
+                write(currPiece + " is not a legal input!");
+                currPiece = readInt(msg);
+            }
+
+            //the choice was legal, so now ask if forward or backward
+            msg = "Now choose whether forward (1) or backward (0). The piece's 'forward' is " + ((currPiece < 0) ? "from bottom to top" : "from top to bottom");
+            int direction = readInt(msg);
+
+            //check if legal
+            while (direction != 0 && direction != 1) {
+                write(direction + " is not a legal input!");
+                direction = readInt(msg);
+            }
+
+            //now we also have the direction, so try to perform the move
+            boolean vorwaerts = (direction == 1);
+
+            //if this returns -1, we do it all again
+            code = setzeZug(currPiece, 1, vorwaerts);
+            if (code == -1) write("You can't move piece " + currPiece + (vorwaerts ? " forward!" : " backward!"));
+        } while (code == -1);
+
+        //bonus round ends the player's turn, so just print the current status
+        System.out.println(output());
     }
 
     /**
@@ -291,7 +350,7 @@ public class Linja extends MiniJava {
         int nextTurn = setzeZug(currPiece, distance, true);
         while (nextTurn == -1) {
             //the player can't do this. A different move has to be done
-            write("You can't move this piece. The destination row is full! Choose a different piece.");
+            write("You can't move this piece! Choose a different piece.");
             currPiece = readInt(msg);
             //check if the choice was legal
             legal = gueltigeEingabe(currPiece, spieler);
@@ -313,11 +372,19 @@ public class Linja extends MiniJava {
         initSpiel();
         System.out.println(output());
 
+        //ask if bonus rule should be used
+        int bonus = readInt("Would you like to use the bonus rule? 1 = yes, 0 = no");
+        //check if legal
+        while (bonus != 0 && bonus != 1) {
+            write(bonus + " is not a legal input!");
+            bonus = readInt("Would you like to use the bonus rule? 1 = yes, 0 = no");
+        }
+
         //randomly pick the player that starts
         boolean player1 = (((int) Math.round(Math.random())) == 1);
 
         while (!spielende()) {
-            spielerZieht(player1 ? 1 : -1);
+            spielerZieht(player1 ? 1 : -1, bonus == 1);
 
             //switch player
             player1 = !player1;
